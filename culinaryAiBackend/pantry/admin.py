5bfd6b1badfc3798e1a91acbec1b92a39dcd3d59
edlawitKd <edelkinfe854@email.com>
2026-02-23 23:10:11 +0300

    Restructure project and add gitignore

--- culinaryAiBackend/pantry/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import PantryItem
-
-@admin.register(PantryItem)
-class PantryItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'quantity', 'user', 'expiry_date')
--- culinaryAiBackend/users/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import generics ,permissions
-from .models import User
-from .serializers import UserSerializer
-
-class UserListView(generics.ListCreateAPIView):
-    queryset=User.objects.all()
-    serializer_class=UserSerializer
-   
